e34f3492e8260693fab3de23dd06132190cf3cb4
Alina Walling <alina.walling@gmail.com>
2021-08-14 16:59:00 -0700

    add script to render menu tabs for each option type

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,24 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    private Animator menuAnimator;
+    public OptionsManager optionsManager;
+
+    public GameObject tabList;
+    public GameObject tabButtonPrefab;
     
+    public GameObject tabContentArea;
+    public GameObject tabPanePrefab;
+
+    public GameObject tabTitle;
+
+    private Animator menuAnimator;
+    private ToggleGroup tabGroup;
+    
+    private bool m_menuIsOpen = false;
+
+    public void ToggleMenu(bool isOpen) {
+        menuAnimator.SetBool("isOpen", isOpen);
+        m_menuIsOpen = isOpen;
+    }
+
+    public void CreateTab(OptionsManager.OptionType type)
+    {
+        GameObject tab = SpawnPrefab(tabButtonPrefab, tabList);
+        Toggle tabToggle = tab.GetComponent<Toggle>();
+        tabToggle.group = tabGroup;
+
+        GameObject tabPane = SpawnPrefab(tabPanePrefab, tabContentArea);
+
+        tabToggle.onValueChanged.AddListener(delegate
+        {
+            if (!tabGroup.AnyTogglesOn() && m_menuIsOpen) ToggleMenu(false);
+            if (tabToggle.isOn && !m_menuIsOpen) ToggleMenu(true);
+            if (tabToggle.isOn) tabTitle.GetComponent<Text>().text = type.ToString();
+            tabPane.SetActive(tabToggle.isOn);
+        });
+    }
+
+    public GameObject SpawnPrefab(GameObject prefab, GameObject parent)
+    {
+        GameObject newObject = GameObject.Instantiate(prefab);
+        newObject.transform.SetParent(parent.transform);
+        newObject.transform.localScale = Vector3.one;
+        newObject.transform.localRotation = Quaternion.identity;
+        return newObject;
+    }
+
+    public void DestroyChildren(GameObject parent)
+    {
+        if (parent.transform.childCount > 0)
+        {
+            foreach (Transform child in parent.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         menuAnimator = GetComponent<Animator>();
-    }
+        tabGroup = tabList.GetComponent<ToggleGroup>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        foreach (OptionsManager.OptionType type in (OptionsManager.OptionType[])System.Enum.GetValues(typeof(OptionsManager.OptionType)))
+        {
+            CreateTab(type);
+        }
 
-    public void ToggleMenu(bool isOpen) {
-        menuAnimator.SetBool("isOpen", isOpen);
+        tabGroup.SetAllTogglesOff();
     }
 }
