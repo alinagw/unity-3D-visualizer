c69dfe54ee09ee2e50ded60d627cc4566036e8e2
Alina Walling <alina.walling@gmail.com>
2021-08-13 23:57:00 -0700

    set up basic 3DVisualizer scene with a plane and Model Controller object and script

---
+++ Assets/ModelController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelController : MonoBehaviour
+{
+
+    public float translateSpeed;
+    public float rotationSpeed;
+    public float scaleSpeed;
+
+    private float yaw;
+    private float pitch;
+    private float roll;
+    private float x;
+    private float y;
+    private float z;
+    private float scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
