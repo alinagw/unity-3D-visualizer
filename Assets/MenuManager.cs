eea7453e16ab4fbc09436d859a35d52635daec14
Alina Walling <alina.walling@gmail.com>
2021-08-14 12:35:00 -0700

    added sidebar menu slide in/out animation and menu show/hide toggle button

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    private Animator menuAnimator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ToggleMenu(bool isOpen) {
+        menuAnimator.SetBool("isOpen", isOpen);
     }
 }
